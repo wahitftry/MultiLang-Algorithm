9b3003833e2251522d381f08c52007e510f1b001
Wahit Fitriyanto <26300406+wahitfitriyanto@users.noreply.github.com>
2023-01-20 18:17:13 +0000

    Inheritance - OOP

---
+++ python/coretan/Inheritance - OOP.py
@@ -0,0 +1,31 @@
+# inheritance memiliki fungsi yang relevance antara parent class dan child class
+
+# Parent Class
+class Animal:
+    def __init__(self):
+        self.tipe = "Animal"
+        self.kecepatan = "Lambat"
+
+    def grow(self):
+        print("Mamalia ini bertumbuh...")
+
+# Child Class
+class Cat(Animal):
+    def __init__(self, nama, tipe):
+
+        super().__init__()
+
+        self.nama = nama
+        self.tipe = tipe
+
+    def run(self):
+        print(f"kucing {self.tipe} ini berlari...")
+
+kinoko = Cat(nama="kinoko", tipe="Anggora")
+minto = Cat(nama="Minto", tipe="Persia")
+
+print(kinoko.kecepatan)
+print(kinoko.tipe)
+
+kinoko.run()
+minto.grow()
