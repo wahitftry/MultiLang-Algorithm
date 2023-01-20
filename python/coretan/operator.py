dbce1f482b76ca6a52b58c24b212d730cb8f6ca1
Wahit Fitriyanto <26300406+wahitfitriyanto@users.noreply.github.com>
2023-01-20 17:30:19 +0000

    operator

---
+++ python/coretan/operator.py
@@ -0,0 +1,48 @@
+print('=====OPERATOR=====')
+print('===Aritmatik Operator===')
+
+a = 5
+b = 3
+print(a + b) 
+print(a - b) 
+print(a * b) 
+print(a / b) 
+print(a % b) 
+print(a // b) 
+print(a ** b) 
+
+print('\n===Aritmatik Operator===')
+
+a = 5 
+a += 3 
+print(a)
+
+a = 5
+a *= 3 
+print(a)
+
+a = 5
+a //=3 
+print(a)
+
+s = "ini adalah string" 
+print(s *2)
+
+b = True 
+print(b/2)
+
+print('\n===Operator Perbandingan===')
+
+
+x = 10
+y = 15
+
+print(x > y) 
+print(x < y) 
+print(x == y)
+print(x != y) 
+print(x >= y)
+print(x <= y)
+
+print(x is y)
+print(x is not y)
