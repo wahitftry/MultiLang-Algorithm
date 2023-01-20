dc0e669dfee4a6305b548c73696f7044ae40a4b5
Wahit Fitriyanto <26300406+wahitfitriyanto@users.noreply.github.com>
2023-01-20 18:10:46 +0000

    Module

---
+++ python/coretan/Module.py
@@ -0,0 +1,21 @@
+print("=====MODULE=====")
+print("=====CREATE OWN MODULE=====")
+
+import playvideo as pv
+
+print(pv.GENRE)
+
+print(pv.DURATION)
+
+pv.play("here with me dav4d")
+
+print("\n=====BUILT-IN MODULE=====")
+
+import os
+import math
+
+from math import pi
+from math import e
+
+print(pi)
+print(e)
