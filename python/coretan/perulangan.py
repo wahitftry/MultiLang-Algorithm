9d5dc152c4490b61bd0b38e4351c18b788de8d47
Wahit Fitriyanto <26300406+wahitfitriyanto@users.noreply.github.com>
2023-01-20 17:50:56 +0000

    perulangan

---
+++ python/coretan/perulangan.py
@@ -0,0 +1,105 @@
+print("=====LOOP=====")
+print("===WHILE LOOP===")
+
+count = 0
+
+while count < 9:
+    print("Nilai count:", count)
+    count = count + 1
+
+print("selamat berjuang!")
+
+print("\n===FOR LOOP===")
+
+
+list_angka = [1, 2, 3, 4, 5]
+
+for x in list_angka:
+    print("intruksi berjalan...")
+    print(x)
+
+print(range(10))
+
+list_range = list(range(10))
+print(list_range)
+
+for x in list(range(2, 20, 3)):
+    print(x)
+
+for x in list(range(1, 11)):
+    print(x)
+
+print("\n===NESTED LOOP===")
+
+
+i = 90
+
+while i < 100:
+
+    j = 2
+    print((i / j))
+    while j < +(i / j):
+        print("loop dalam loop!")
+        j = j + 1
+        i = i + 1
+
+print("Selamat Berjuang")
+
+print("\n===BREAK & CONTINUE===")
+
+
+for val in "string":
+
+    if val == "g":
+        continue
+
+    print(val)
+
+
+for val in "string":
+
+    if val == "i":
+        break
+
+    print(val)
+
+print("loop telah berakhir.")
+
+print("\n===FOR ELSE===")
+
+daftar_murid = ["Angga", "Mardadi", "Rowi"]
+
+nama_murid = "Jaka"
+
+for nama in daftar_murid:
+
+    if nama == nama_murid:
+        print((nama))
+        break
+
+else:
+    print("nama murid tidak ditemukan")
+
+
+daftar_murid = ["Angga", "Mardadi", "Rowi"]
+
+nama_murid = "Angga"
+
+for nama in daftar_murid:
+
+    if nama == nama_murid:
+        print((nama))
+        break
+
+else:
+    print("nama murid tidak ditemukan")
+
+
+for nama in daftar_murid:
+    pass
+
+if daftar_murid == 0:
+    pass
+
+else:
+    pass
