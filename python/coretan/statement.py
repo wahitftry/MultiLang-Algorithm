4f1ce0e97784b3201d106c4ceb05e6045d62ff11
Wahit Fitriyanto <26300406+wahitfitriyanto@users.noreply.github.com>
2023-01-20 17:08:20 +0000

    file statement

---
+++ python/coretan/statement.py
@@ -0,0 +1,14 @@
+print("Halo")
+
+print("Nama saya Wahit Fitriyanto")
+print("Ini adalah coretan pertama")
+print("Kalo ada yg salah harap dimaklumi")
+
+max_limit = 1
+
+for i in range(1, max_limit):
+    print(i)
+    if i == 5:
+        break
+
+if True: print("Hello"); a = 5
