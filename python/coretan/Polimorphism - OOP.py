ec86ec26784d30eeac8f384dc28d5cce3e7ea30a
Wahit Fitriyanto <26300406+wahitfitriyanto@users.noreply.github.com>
2023-01-20 18:18:45 +0000

    Polimorphism - OOP

---
+++ python/coretan/Polimorphism - OOP.py
@@ -0,0 +1,25 @@
+# bisa menjalankan fungsi yang sama dari objek yang berbeda
+class Cat:
+    def __init__(self):
+        self.nama = "Meong"
+        self.tipe = "Anggora"
+
+    def forward(self):
+        print("Kucing berlari...")
+
+class Bird:
+    def __init__(self):
+        self.nama = "Erago"
+        self.tipe = "Elang"
+
+    def forward(self):
+        print("Burung terbang...")
+
+def melaju(objek):
+    objek.forward()
+
+meong = Cat()
+erago = Bird()
+
+melaju(meong)
+melaju(erago)
