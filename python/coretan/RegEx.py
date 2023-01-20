4497c5671d911e010376c62e1f06de9e2877b7e0
Wahit Fitriyanto <26300406+wahitfitriyanto@users.noreply.github.com>
2023-01-20 18:13:48 +0000

    RegEx

---
+++ python/coretan/RegEx.py
@@ -0,0 +1,50 @@
+print("=====RegEx=====")
+
+import re
+
+print("\n=====MATCH RegEx=====")
+teks = "regex"
+x = re.match("^r...x$", teks)
+print(x)
+
+print("\n=====SPLIT RegEx=====")
+teks = "i'm happy learn regex"
+x = re.split("\s", teks)
+print(x)
+
+print("\n=====SUB RegEx=====")
+teks = """
+        the 3 loop type in language python programming are while loop,
+        for loop and nested loop 2022
+        """
+x = re.sub("\d+", "", teks)
+print(x)
+
+print("\n=====SEARCH RegEx=====")
+teks = "hujan deras di kawasan depok"
+result = re.search("^hujan.*depok$", teks)
+
+if result:
+    print("YES! we have a match.")
+else:
+    print("No match.")
+
+print("\n=====FINDALL RegEx=====")
+teks = "23 oct 2019 23 oct,2019 23 october,2019 oct 26,2020"
+x = re.findall("\d{2} [a-z]{3} \d{4}", teks)
+print(x)
+
+print("\n=====SUB RegEx=====")
+teks = "Harga 1 mobil antik tersebut yaitu $1000"
+x = re.sub("\$\d+", "_", teks)
+print(x)
+
+print("\n=====SUB RegEx=====")
+teks = "Akan dialihkan ke http://medium.com"
+x = re.sub("http[s]?\://\S+", "_", teks)
+print(x)
+
+print("\n=====FINDALL RegEx=====")
+teks = "Luar biasa! Banyak anak-anak muda traveling tahun ini, begini tanggapan lesti"
+x = re.findall
+print(x)
