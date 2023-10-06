ed55caba4c72efc9f408559ab785b10493ba9c51
Wahit Fitriyanto <26300406+wahitftry@users.noreply.github.com>
2023-10-06 09:42:43 +0700

    added few files

---
+++ python/Basic/add 2 numbers.py
@@ -0,0 +1,6 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+sum = num1 + num2
+
+print("The sum of {0} and {1} is {2}".format(num1, num2, sum))
---
+++ python/Basic/find max numbers.py
@@ -0,0 +1,9 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+if num1 > num2:
+    max_num = num1
+else:
+    max_num = num2
+
+print("The maximum of {0} and {1} is {2}".format(num1, num2, max_num))
