bb09e0a0f290b1a97504aa02ff62cd8b22918699
Wahit Fitriyanto <26300406+wahitftry@users.noreply.github.com>
2023-10-08 00:32:45 +0700

    fix some issues on armstrong number checker

--- python/Basic/armstrong number checker.py
+++ python/Basic/armstrong number checker.py
@@ -1,16 +1,14 @@
-def is_armstrong(num_str):
+def is_armstrong(num):
+    num_str = str(num)
     num_digits = len(num_str)
     sum = 0
     for digit in num_str:
         sum += int(digit) ** num_digits
     
-    if sum == int(num_str):
-        return True
-    else:
-        return False
+    return bool(sum == num)
 
-num_str = input("Enter a number: ")
-if is_armstrong(num_str):
-    print("{0} is an Armstrong number".format(num_str))
+num = int(input("Enter a number: "))
+if is_armstrong(num):
+    print("{0} is an Armstrong number".format(num))
 else:
-    print("{0} is not an Armstrong number".format(num_str))
+    print("{0} is not an Armstrong number".format(num))
