2f52e681e3ac16d2d228ff9b2a9a90ff32a675b6
Wahit Fitriyanto <26300406+wahitftry@users.noreply.github.com>
2023-10-08 00:28:34 +0700

    Redefining name 'num' from outer scope

--- python/Basic/armstrong number checker.py
+++ python/Basic/armstrong number checker.py
@@ -1,17 +1,16 @@
-def is_armstrong(num):
-    num_str = str(num)
+def is_armstrong(num_str):
     num_digits = len(num_str)
     sum = 0
     for digit in num_str:
         sum += int(digit) ** num_digits
     
-    if sum == num:
+    if sum == int(num_str):
         return True
     else:
         return False
 
-num = int(input("Enter a number: "))
-if is_armstrong(num):
-    print("{0} is an Armstrong number".format(num))
+num_str = input("Enter a number: ")
+if is_armstrong(num_str):
+    print("{0} is an Armstrong number".format(num_str))
 else:
-    print("{0} is not an Armstrong number".format(num))
+    print("{0} is not an Armstrong number".format(num_str))
