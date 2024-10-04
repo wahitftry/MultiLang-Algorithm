76d515c6bb5597e0f93f0d509ecc7d61fd12f02e
Wahit Fitriyanto <26300406+wahitftry@users.noreply.github.com>
2024-10-04 13:20:48 +0000

    update

---
+++ leetcode/N-th Tribonacci Number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+        if (n == 0) return 0;
+        if (n == 1 || n == 2) return 1;
+        
+        int t0 = 0, t1 = 1, t2 = 1, tn;
+        
+        for (int i = 3; i <= n; i++) {
+            tn = t0 + t1 + t2;
+            t0 = t1;
+            t1 = t2;
+            t2 = tn;
+        }
+        
+        return tn;
+    }
+};
